2bf11b90cf65e5936a008c31c8993b48194bc1a7
Abdulaziz Egamberdiyev <145344817+abdulaziz0413@users.noreply.github.com>
2024-02-09 12:26:18 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using Npgsql;
+
+namespace MyADONETProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string CONNECTIONSTRING= "Server=localhost;Port=5432;Database=vs;User Id=postgres;Password=oktava;";
+            NpgsqlConnection connection = new NpgsqlConnection(CONNECTIONSTRING);
+            connection.Open();
+            string query = "select * from skypedars";
+            
+            NpgsqlCommand command = new NpgsqlCommand(query, connection);
+            NpgsqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine("{0},{1},{")
+            }
+            //logic
+            Console.WriteLine("Database da table yaratildi");
+
+            connection.Close();
+        }
+    }   
+}
