ecafdd218578fee5a4c32de4567c38cd98284497
Abdulaziz Egamberdiyev <145344817+abdulaziz0413@users.noreply.github.com>
2024-02-12 09:40:38 +0500

    all dates were changed

--- Program.cs
+++ Program.cs
@@ -1,57 +1,207 @@
-﻿using Npgsql;
+﻿using System;
+using System.Collections.Generic;
+using Npgsql;
 
-namespace MyADONETProject
+class Program
 {
-    public class insertOneData
+    static string connString = "Server=localhost;Port=5432;Database=vs;User Id=postgres;Password=oktava;";
+
+    static void Main()
     {
-        
-        public insertOneData()
+        CreateTable();
+        InsertData();
+        InsertMultipleData();
+        GetAllData();
+        GetById(1);
+        DeleteById(1);
+        UpdateById(2);
+        UpdateColumnById(3);
+        SearchData("searchtext");
+        AddNewColumn("newcolumn");
+        AddNewColumnWithDefault("newcolumnwithdefault", "defaultvalue");
+        UpdateColumnName("oldcolumnname", "newcolumnname");
+        UpdateTableName("newtablename");
+    }
+
+    static void CreateTable()
+    {
+        using (NpgsqlConnection conn = new NpgsqlConnection(connString))
         {
-            SkypeDars table = new SkypeDars();
-            Console.WriteLine("mavzuni qo'shing");
-            table.mavzu = Console.ReadLine();
-            Console.WriteLine("o'quvshilar sonini qo'shing");
-            table.students_count = int.Parse(Console.ReadLine());
-            table.start_date = DateTime.UtcNow;
-            string query = $@"insert into skypedars(mavzu, start_date, students_count) 
-                            values ('{table.mavzu}',{table.start_date}, {table.students_count})";
-
-            return query;
-
-
-
+            conn.Open();
+            using (NpgsqlCommand cmd = new NpgsqlCommand("CREATE TABLE IF NOT EXISTS mytable (id SERIAL PRIMARY KEY, name TEXT)", conn))
+            {
+                cmd.ExecuteNonQuery();
+            }
         }
     }
-    
-    internal class Program
+
+    static void InsertData()
     {
-        static void Main(string[] args)
+        using (NpgsqlConnection conn = new NpgsqlConnection(connString))
         {
-            string CONNECTIONSTRING= "Server=localhost;Port=5432;Database=vs;User Id=postgres;Password=oktava;";
-            NpgsqlConnection connection = new NpgsqlConnection(CONNECTIONSTRING);
-            int son;
-            Console.WriteLine("Tegishli sonni tanlang:\n\t1-Bitta ma'lumot qo'shish\n\t2-Ko'p ma'lumot qo'shish");
-            son = int.Parse(Console.ReadLine());
-            switch (son)
+            conn.Open();
+            using (NpgsqlCommand cmd = new NpgsqlCommand("INSERT INTO mytable (name) VALUES ('data1')", conn))
             {
-                case 1:
-                    {
-                      
-                        
-                        break;
-                    }
-            connection.Open();
-
-
-            
-            
-            NpgsqlCommand command = new NpgsqlCommand(insertOneData., connection);
-            NpgsqlDataReader reader = command.ExecuteReader();
-             
-            //logic
-            Console.WriteLine("Database da table yaratildi");
-
-            connection.Close();
+                cmd.ExecuteNonQuery();
+            }
         }
-    }   
-}
+    }
+
+    static void InsertMultipleData()
+    {
+        using (NpgsqlConnection conn = new NpgsqlConnection(connString))
+        {
+            conn.Open();
+            using (NpgsqlCommand cmd = new NpgsqlCommand("INSERT INTO mytable (name) VALUES ('data2'), ('data3')", conn))
+            {
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+
+    static void GetAllData()
+    {
+        using (NpgsqlConnection conn = new NpgsqlConnection(connString))
+        {
+            conn.Open();
+            using (NpgsqlCommand cmd = new NpgsqlCommand("SELECT * FROM mytable", conn))
+            {
+                using (NpgsqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine(reader.GetString(0)); 
+                        Console.WriteLine(reader.GetString(1)); 
+                    }
+                }
+            }
+        }
+    }
+
+    static void GetById(int id)
+    {
+        using (NpgsqlConnection conn = new NpgsqlConnection(connString))
+        {
+            conn.Open();
+            using (NpgsqlCommand cmd = new NpgsqlCommand("SELECT * FROM mytable WHERE id = @id", conn))
+            {
+                cmd.Parameters.AddWithValue("id", id);
+                using (NpgsqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine(reader.GetString(0)); 
+                        Console.WriteLine(reader.GetString(1)); // Assuming the second column is name
+                    }
+                }
+            }
+        }
+    }
+
+    static void DeleteById(int id)
+    {
+        using (NpgsqlConnection conn = new NpgsqlConnection(connString))
+        {
+            conn.Open();
+            using (NpgsqlCommand cmd = new NpgsqlCommand("DELETE FROM mytable WHERE id = @id", conn))
+            {
+                cmd.Parameters.AddWithValue("id", id);
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+
+    static void UpdateById(int id)
+    {
+        using (NpgsqlConnection conn = new NpgsqlConnection(connString))
+        {
+            conn.Open();
+            using (NpgsqlCommand cmd = new NpgsqlCommand("UPDATE mytable SET name = 'updateddata' WHERE id = @id", conn))
+            {
+                cmd.Parameters.AddWithValue("id", id);
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+
+    static void UpdateColumnById(int id)
+    {
+        using (NpgsqlConnection conn = new NpgsqlConnection(connString))
+        {
+            conn.Open();
+            using (NpgsqlCommand cmd = new NpgsqlCommand("UPDATE mytable SET name = 'updateddata' WHERE id = @id", conn))
+            {
+                cmd.Parameters.AddWithValue("id", id);
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+
+    static void SearchData(string searchText)
+    {
+        using (NpgsqlConnection conn = new NpgsqlConnection(connString))
+        {
+            conn.Open();
+            using (NpgsqlCommand cmd = new NpgsqlCommand("SELECT * FROM mytable WHERE name LIKE @searchText", conn))
+            {
+                cmd.Parameters.AddWithValue("searchText", "%" + searchText + "%");
+                using (NpgsqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine(reader.GetString(0)); // Assuming the first column is id
+                        Console.WriteLine(reader.GetString(1)); // Assuming the second column is name
+                    }
+                }
+            }
+        }
+    }
+
+    static void AddNewColumn(string columnName)
+    {
+        using (NpgsqlConnection conn = new NpgsqlConnection(connString))
+        {
+            conn.Open();
+            using (NpgsqlCommand cmd = new NpgsqlCommand($"ALTER TABLE mytable ADD COLUMN {columnName} TEXT", conn))
+            {
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+
+    static void AddNewColumnWithDefault(string columnName, string defaultValue)
+    {
+        using (NpgsqlConnection conn = new NpgsqlConnection(connString))
+        {
+            conn.Open();
+            using (NpgsqlCommand cmd = new NpgsqlCommand($"ALTER TABLE mytable ADD COLUMN {columnName} TEXT DEFAULT '{defaultValue}'", conn))
+            {
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+
+    static void UpdateColumnName(string oldColumnName, string newColumnName)
+    {
+        using (NpgsqlConnection conn = new NpgsqlConnection(connString))
+        {
+            conn.Open();
+            using (NpgsqlCommand cmd = new NpgsqlCommand($"ALTER TABLE mytable RENAME COLUMN {oldColumnName} TO {newColumnName}", conn))
+            {
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+
+    static void UpdateTableName(string newTableName)
+    {
+        using (NpgsqlConnection conn = new NpgsqlConnection(connString))
+        {
+            conn.Open();
+            using (NpgsqlCommand cmd = new NpgsqlCommand($"ALTER TABLE mytable RENAME TO {newTableName}", conn))
+            {
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}   
